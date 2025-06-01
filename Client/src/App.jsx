c58e4d75956e2762c7a392d951ece0a8575a54bd
Your Name <pankaj23.mdev@gmail.com>
2025-06-01 15:59:16 +0530

    created the project

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+// import './App.css'/
+
+function App() {
+
+
+  return (
+     <div>
+      <h2  className='text-red-700 font-bold '>heloo welcome</h2>
+     </div>
+  )
+}
+
+export default App
